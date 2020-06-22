84fc4eaf8fd3c835c2dc593af0ef12f4fdcf3cd2
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-06-22 11:13:37 -0400

    Build Your App

--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,10 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder(PrefixedReminder):
+    def __init__(self, text ,date = None):
+        super().__init__("please")
+        self.text = self.prefix + text
+    def __iter(self):
+        return iter([self.text])
